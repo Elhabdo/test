ca49a20de0420035e6989a0a5ae9c116efae77fa
Abdelmounaim Elhaila <abdelmounaimelhaila@gmail.com>
2021-03-09 21:32:44 +0100

    first commit

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', 'localhost'),
+  port: env.int('PORT', 9696),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'acd0e1abc7b133f63357c91d9a33b568'),
